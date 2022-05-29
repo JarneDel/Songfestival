c217684afd35902cc4fadea9111799f4e6858e26
Delarue Jarne <91070860+JarneDel@users.noreply.github.com>
2022-05-29 21:14:37 +0200

    Update DATABASE.sql

--- database/DATABASE.sql
+++ database/DATABASE.sql
@@ -1,21 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.37, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: songfestivaldb
+-- Host: localhost    Database: songfestivaldb
 -- ------------------------------------------------------
--- Server version	8.0.28
-DROP DATABASE IF EXISTS songfestivaldb;
-CREATE DATABASE IF NOT EXISTS `songfestivaldb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-use `songfestivaldb`;
-
-CREATE USER IF NOT EXISTS 'root_songfestival'@'localhost' IDENTIFIED BY 'root_songfestival';
-GRANT ALL PRIVILEGES ON *.* TO 'root_songfestival'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
+-- Server version	5.5.5-10.5.15-MariaDB-0+deb11u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -29,16 +21,25 @@ FLUSH PRIVILEGES;
 
 DROP TABLE IF EXISTS `liedjes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `liedjes` (
-  `idLiedjes` int NOT NULL AUTO_INCREMENT,
+  `idLiedjes` int(11) NOT NULL AUTO_INCREMENT,
   `Artiest` varchar(90) NOT NULL,
   `Titel` varchar(90) NOT NULL,
   `InleverDatum` datetime DEFAULT NULL,
   `URL` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idLiedjes`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `liedjes`
+--
+
+LOCK TABLES `liedjes` WRITE;
+/*!40000 ALTER TABLE `liedjes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `liedjes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `rating`
@@ -46,16 +47,25 @@ CREATE TABLE `liedjes` (
 
 DROP TABLE IF EXISTS `rating`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rating` (
-  `idrating` int NOT NULL AUTO_INCREMENT,
+  `idrating` int(11) NOT NULL AUTO_INCREMENT,
   `rating` varchar(45) DEFAULT NULL,
-  `idLiedje` int DEFAULT NULL,
+  `idLiedje` int(11) DEFAULT NULL,
   PRIMARY KEY (`idrating`),
   KEY `idLiedjes_idx` (`idLiedje`),
   CONSTRAINT `idLiedjes` FOREIGN KEY (`idLiedje`) REFERENCES `liedjes` (`idLiedjes`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -65,3 +75,5 @@ CREATE TABLE `rating` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-29 21:12:33
